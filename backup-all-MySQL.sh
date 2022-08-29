7c7852421ef42368e10550b23aec8668c489192a
Buzzman25 <Buzzman25@users.noreply.github.com>
2022-08-29 00:25:07 -0700

    Update backup-all-MySQL.sh to remove password requirement in file

--- backup-all-MySQL.sh
+++ backup-all-MySQL.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
-
-host="yourMySQLHostname"
+#!/bin/bash
+# Read Password
+echo -n Password:
+read -s password
+echo
+[[ -z "$password" ]] && { echo "Password is empty" ; exit 1; }
+# Run Command
+host="hostname"
 username="username"
-password="password"
 
 for DB in $(mysql --host="$host" --user="$username" -p"$password" -e"show databases" --skip-column-names -s | grep -v information_schema);
 do
+  echo Backing up $DB;
   mysqldump --opt --host="$host" --user="$username" -p"$password" --no-tablespaces $DB | gzip > ./`date +%Y-%m-%d_%H.%M.%S`_$DB.sql.gz;
 done
